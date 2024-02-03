440fe427164e7c433d5595cf3bb558397c5f7135
nocturnalnerds <133097737+nocturnalnerds@users.noreply.github.com>
2024-02-03 21:01:27 +0000

    #

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -5,30 +5,38 @@ function Footer() {
     <footer className="text-center text-lg-start">
   <div className="container p-4">
     <div className="row">
-    <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-                <h5 className="text-uppercase">By BRUH Developer</h5>
+      <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
+        <h5 className="text-uppercase">By BRUH Developer</h5>
         <ul>
-          <li>
-            <a className="text-white" href="https://www.fathin.my.id">M.Fathin Halim</a>
-          </li>
-          <li>
-            Ziddan (nanti nama lengkap)
-          </li>
-          <li>
-            Zidan (nanti lengkap juga)
-          </li>
-          <li>
-            Will (Nanti lengkap juga)
-          </li>
+            <li>
+                <a className="text-white"  href="https://www.fathin.my.id">M.Fathin Halim</a>
+            </li>
+            <li>
+                Ziddan (nanti nama lengkap)
+            </li>
+            <li>
+                Zidan (nanti lengkap juga)
+            </li>
+            <li>
+                Will (Nanti lengkap juga)
+            </li>
         </ul>
       </div>
-      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-        <img src="https://ik.imagekit.io/9hpbqscxd/SG/image-39.jpg?updatedAt=1705798245623" style={{width: "140px"}}/>
+      <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
+        <div className="d-lg-flex">
+        <img className="mr-3 mb-3 mb-lg-0" src="https://ik.imagekit.io/9hpbqscxd/SG/image-39.jpg?updatedAt=1705798245623" style={{width: "140px"}}/>
+        <div>
+        <h5 className="text-uppercase">Who's Bruh Developer</h5>
+        <p>
+          Bruh Developer (Brogrammer Road To Unusual Hacker) is a programmer teams for improve the team skill and experience especialy at hackaton
+        </p>
+        </div>
+        </div>
       </div>
     </div>
   </div>
-</footer>
 
+</footer>
 
 );
 }

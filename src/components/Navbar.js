f2ca61b94bb1b91cd2b98049a8032892bc0cf1e7
DomaTomoharu <halimfathin7@gmail.com>
2024-02-04 17:21:02 +0700

    fix post

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,30 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { authOptions } from "@/utils/auth"
-import { getServerSession } from "next-auth/next"
+import "bootstrap/dist/css/bootstrap.min.css";
+import { authOptions } from "@/utils/auth";
+import { getServerSession } from "next-auth/next";
 
+//sign in nya dimana
+// agak laen lu naruh nya
+// dom bantu bentar ng
+
+// lu ctrl space si func signin dibawah aja biar autoimport
+//dari mana dia
+//GAK ADA CO
+// gw liat error nya dia gabisa ditaruh langsung gitubgw search bentar
 function MyNavbar() {
   return (
     <nav className="navbar navbar-dark p-2 sticky-top">
-        <a className="navbar-brand" href="/">Renew Planet</a>
-        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Login</button>
-    </nav>  
-);
+      <a className="navbar-brand" href="/">
+        Renew Planet
+      </a>
+      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
+        Login
+      </button>
+    </nav>
+  );
 }
 
 export default MyNavbar;
 
 export async function getServerSideProps() {
-  const session = await getServerSession(authOptions)
+  const session = await getServerSession(authOptions);
 }
